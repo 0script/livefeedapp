1265d140c141b499522d28c8f556a359bff48d91
0script <obmco76@gmail.com>
2022-03-28 19:13:57 +0200

    Login and register view finish

---
+++ src/livefeedapp/feedapp/forms.py
@@ -0,0 +1,119 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=UserCreationForm.Meta.fields+('tel',)
+
+class LoginForm(forms.Form):
+    username=forms.CharField(max_length=150,
+        required=True,
+        label='Username',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'Type your username',
+                'type':'text',
+                'class':'input',
+            }
+        )
+    )
+    password=forms.CharField(label='Password', widget=forms.PasswordInput)
+
+
+# class CustomUserCreationForm(UserCreationForm):
+
+#     username=forms.CharField(max_length=150,
+#         required=True,
+#         label='Username',
+#         widget=forms.TextInput(
+#             attrs={
+#                 'placeholder':'Type your username',
+#                 'type':'text',
+#                 'class':'input',
+#             }
+#         )
+#     )
+
+#     email=forms.EmailField(
+#         required=False,
+#         label='Email',
+#         widget=forms.EmailInput(
+#             attrs={
+#                 'placeholder':'Type Your email',
+#                 'type':'text',
+#                 'class':'email',
+#             }
+#         )
+#     )
+
+#     tel=forms.CharField(max_length=150,
+#         required=True,
+#         label='Tel',
+#         widget=forms.TextInput(
+#             attrs={
+#                 'placeholder':'Your Phone Number',
+#                 'type':'text',
+#                 'class':'input',
+#             }
+#         )
+#     )
+
+
+#     # tel=forms.RegexField(
+#     #     regex=r'^\+?1?\d{9,15}$',
+#     #     required=False,
+#     #     widget=forms.TextInput(
+#     #         attrs={
+#     #             'placeholder':'Your Phone Number',
+#     #             'type':'text',
+#     #             'class':'input',
+#     #         }
+#     #     )
+#     # )
+
+#     class Meta:
+#         model = User
+#         fields = ('username','email','tel','password')
+
+#     def username_clean(self):
+#         username=self.cleaned_data['username'].lower()
+#         new=User.objects.filter(username=username)
+        
+#         if new.count():
+#             raise ValidationError('Username Already Exist')
+#         return username
+
+#     def email_clean(self):
+#         email=self.cleaned_data['email'].lower()
+#         new=User.objects.filter(email=email)
+#         if new.count():
+#             raise ValidationError('Email Already Exist')
+#         return email
+    
+#     def tel_clean(self):
+#         tel=self.cleaned_data['tel'].lower()
+#         new=User.objects.filter(tel=tel)
+#         if new.count():
+#             raise ValidationError('Phone number already exist')
+#         return tel
+
+#     # def password1_clean(self):
+#     #     password1=self.cleaned_data['password1']
+#     #     password2=self.cleaned_data['password2']
+
+#     #     if password1 and password2 and password1!=password2:
+#     #         raise ValidationError('Password do not match')
+#     #     return password2
+
+#     def save(self,commit=True):
+#         user=CustomUser.objects.create_user(
+#             self.cleaned_data['username'],
+#             self.cleaned_data['email'],
+#             self.cleaned_data['tel'],
+#             self.cleaned_data['passwrod']
+#         )
+
+#         return user
--- src/livefeedapp/feedapp/views.py
+++ src/livefeedapp/feedapp/views.py
@@ -1,3 +1,112 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import CustomUserCreationForm,LoginForm
+from django.contrib import messages
+from django.contrib.auth import logout,authenticate,login
+from .models import User
+from django.http import HttpRequest,HttpResponseRedirect
+
 
 # Create your views here.
+
+def index_view(request,pk='',*args, **kwargs):
+    template_name='feedapp/index.html'
+
+    if request.user.is_authenticated:
+        context={
+            'username':request.user.username,
+        }
+    else:
+        context={
+            'username':'',
+        }
+    return render(request, template_name)
+
+def register_view(request,*args, **kwargs):
+    template_name='feedapp/register.html'
+    errors=''
+
+    print('start')
+    if request.method == 'POST':
+        form=CustomUserCreationForm(request.POST)
+        print('post')
+        if form.is_valid():
+            form.save()
+            print('save')
+            messages.success(request,'Account created successfully')
+            name_user=form['username'].value()
+
+            req=HttpRequest()
+            req.method="GET"
+            req.user=User.objects.filter(username=name_user)
+
+            return redirect(index_view)
+        errors=form.errors
+    
+
+    form=CustomUserCreationForm()
+
+    context={
+        'form':form,
+        'errors':errors,
+    }
+
+    return render(request,template_name,context)
+
+def logout_view(request):
+    logout(request)
+    return redirect(index_view)
+
+
+def login_view(request,*args, **kwargs):
+    template_name='feedapp/login.html'
+    
+    print('login')
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        print('post')
+        if form.is_valid():
+            print('is valid')
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=authenticate(request,username=username,password=password)     
+            print('login')
+            print(username)
+            print(password)
+            if user is not None:
+                login(request,user)
+                print('uset is not none')
+                return redirect(index_view)
+                
+        
+    form=LoginForm()
+    print('new form')
+    context={
+        'form':form,
+        'error':True,
+    }
+    return render(request, template_name,context)
+    
+    form=LoginForm()
+    print(form)
+    context={
+        'form':form,
+        'error':False,
+    }
+    print('exit render')
+    return render(request, template_name,context)
+    
+    # username=request.POST.get('username')
+    # password=request.POST.get('password')
+    # user=authenticate(request,username=username,password=password)
+    # print('login')
+    # print(username)
+    # print(password)
+    # if user is not None:
+    #     login(request,user)
+    #     redirect(index_view)
+    # else:
+    #     context={
+    #         'error':True
+    #     }
+
+    # return render(request, template_name,context=context)
