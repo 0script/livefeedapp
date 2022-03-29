9057918a3256202cc14c8ad9d4566b3a8bee9913
0script <obmco76@gmail.com>
2022-03-29 05:24:32 +0200

    Registration Login Logout Done

--- src/livefeedapp/feedapp/views.py
+++ src/livefeedapp/feedapp/views.py
@@ -25,24 +25,24 @@ def register_view(request,*args, **kwargs):
     template_name='feedapp/register.html'
     errors=''
 
-    print('start')
     if request.method == 'POST':
         form=CustomUserCreationForm(request.POST)
-        print('post')
         if form.is_valid():
             form.save()
-            print('save')
             messages.success(request,'Account created successfully')
             name_user=form['username'].value()
-
-            req=HttpRequest()
-            req.method="GET"
-            req.user=User.objects.filter(username=name_user)
-
+            passwd=form['password1'].value()
+            user=authenticate(username=name_user,password=passwd)
+        
+            login(request,user)
             return redirect(index_view)
+            # req=HttpRequest()
+            # req.method="GET"
+            # req.user=User.objects.filter(username=name_user)
+
+            # return redirect(index_view)
         errors=form.errors
     
-
     form=CustomUserCreationForm()
 
     context={
@@ -60,53 +60,20 @@ def logout_view(request):
 def login_view(request,*args, **kwargs):
     template_name='feedapp/login.html'
     
-    print('login')
     if request.method=='POST':
         form=LoginForm(request.POST)
-        print('post')
         if form.is_valid():
-            print('is valid')
             username=form.cleaned_data['username']
             password=form.cleaned_data['password']
             user=authenticate(request,username=username,password=password)     
-            print('login')
-            print(username)
-            print(password)
+            
             if user is not None:
                 login(request,user)
-                print('uset is not none')
                 return redirect(index_view)
-                
-        
+
     form=LoginForm()
-    print('new form')
     context={
         'form':form,
         'error':True,
     }
     return render(request, template_name,context)
-    
-    form=LoginForm()
-    print(form)
-    context={
-        'form':form,
-        'error':False,
-    }
-    print('exit render')
-    return render(request, template_name,context)
-    
-    # username=request.POST.get('username')
-    # password=request.POST.get('password')
-    # user=authenticate(request,username=username,password=password)
-    # print('login')
-    # print(username)
-    # print(password)
-    # if user is not None:
-    #     login(request,user)
-    #     redirect(index_view)
-    # else:
-    #     context={
-    #         'error':True
-    #     }
-
-    # return render(request, template_name,context=context)
